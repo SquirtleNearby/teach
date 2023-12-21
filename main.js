ca1739daa0033b4fab70dc5f2e3ee78fb4272ed6
SquirtleNearby <aziankev23101999@gmail.com>
2023-12-21 15:24:05 +0500

    Убрал ошибочно вставочный js код

--- main.js
+++ main.js
@@ -2,11 +2,3 @@ import 'reset-css'
 import './style.css'
 
 
-// Задача с превращением первых символов строки в заглавные буквы
-// let text = "How can mrrrors be real if our eyes aren't real"
-// function Uppercase(row) {
-//   return row.split(" ").map((word) => {
-//     return word.charAt(0).toUpperCase() + word.slice(1)
-//   }).join(' ')
-// }
-// console.log(Uppercase(text));
